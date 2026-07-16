e73e5c988c9d32d820155684a8d92be2a92e0584
Francisss9 <fpsgomes9@gmail.com>
2026-07-16 21:41:02 +0100

    Updated app to work with accept different OS's for 'npm run make'

--- forge.config.js
+++ forge.config.js
@@ -9,18 +9,21 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
+      platforms: ['win32'],
       config: {},
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'linux', 'win32'],
     },
     {
       name: '@electron-forge/maker-deb',
+      platforms: ['linux'],
       config: {},
     },
     {
       name: '@electron-forge/maker-rpm',
+      platforms: ['linux'],
       config: {},
     },
   ],
